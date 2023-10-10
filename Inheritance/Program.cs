5bbf857fd549e3e042ad9aba4719a76b5aa3c1eb
b-juu <erichassett4@gmail.com>
2023-10-10 11:11:10 -0400

    initial commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -28,12 +28,42 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird bird = new Bird
+            {
+                Name = "Eagle",
+                Age = 5,
+                Habitat = "Mountains",
+                IsPredator = true,
+                FeatherColor = "Brown",
+                CanFly = true,
+                Wingspan = 6,
+                HasBeak = true
+            };
+            Console.WriteLine($"Bird Information:");
+            Console.WriteLine($"This bird is an {bird.Name} and is {bird.Age} years old. It can be found in {bird.Habitat}. It is {(bird.IsPredator ? "a predator" : "not a predator")}.");
+            Console.WriteLine($"Its feathers are {bird.FeatherColor}, and it {(bird.CanFly ? "can" : "cannot")} fly. Its wingspan is {bird.Wingspan} inches, and it {(bird.HasBeak ? "has" : "does not have")} a beak.");
+
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+
+            Reptile reptile = new Reptile
+            {
+                Name = "Crocodile",
+                Age = 10,
+                Habitat = "Swamps and Rivers",
+                IsPredator = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                CanRegenerate = false,
+                LaysEggs = true
+            };
+            Console.WriteLine("\nReptile Information:");
+            Console.WriteLine($"This reptile is a {reptile.Name} and is {reptile.Age} years old. It typically inhabits {reptile.Habitat} and it is {(reptile.IsPredator ? "an apex predator" : "not a predator")}, and it is {(reptile.IsColdBlooded ? "cold-blooded" : "warm-blooded")}. It {(reptile.HasScales ? "has" : "does not have")} scales.");
+            Console.WriteLine($"It can {(reptile.CanRegenerate ? "regenerate" : "not regenerate")} and {(reptile.LaysEggs ? "lays eggs" : "gives live birth")}.");
         }
     }
 }
